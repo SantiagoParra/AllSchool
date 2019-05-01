24f14a3ef10b62a137309eb8263322a17bb9034e
Daniela <danicasta29@hotmail.com>
2019-05-01 10:57:12 -0500

    Splash

---
+++ app/src/main/java/com/example/allschool/Splash.java
@@ -0,0 +1,27 @@
+package com.example.allschool;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);//dure dos segundos y luego desaparezca
+    }
+ }
