992cdaddfcb834990645fd99ff0e582706074dfc
Daniela <danicasta29@hotmail.com>
2019-05-02 19:20:17 -0500

    dialog nota

--- app/src/main/java/com/example/allschool/MainActivity.java
+++ app/src/main/java/com/example/allschool/MainActivity.java
@@ -1,28 +1,45 @@
 package com.example.allschool;
 
-import android.content.ClipData;
 import android.content.Intent;
-import android.media.Image;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatDelegate;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.Switch;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+ //  private ImageView iv_materias;
+   //private ImageView iv_tareas;
+    //private ImageView iv_horario;
+    //private ImageView iv_gps;
+    private ImageView iv_calculoNota;
+
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        iv_calculoNota = (ImageView)findViewById(R.id.img_view_nota);
+
+        iv_calculoNota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+                View mView =getLayoutInflater().inflate(R.layout.dialog_nota, null);
+                TextView tv_periodo1 = (TextView) findViewById(R.id.tv_periodo1);
+                TextView tv_periodo2 = (TextView) findViewById(R.id.tv_periodo2);
+               // TextView tv_periodo3 = (TextView) findViewById(R.id.tv_periodo3);
+
+            mBuilder.setView(mView);
+            AlertDialog alert = mBuilder.create();
+            alert.show();
+            }
+        });
         //boton ajustes en el action bar
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.btn_ajustes);
