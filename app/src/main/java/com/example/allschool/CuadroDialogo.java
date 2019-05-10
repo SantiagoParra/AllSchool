5cb833417e2865d82f759478952ff141bf42af42
Unknown <oparram@unbosque.edu.co>
2019-05-10 01:05:44 -0500

    Validacion

--- app/src/main/java/com/example/allschool/CuadroDialogo.java
+++ app/src/main/java/com/example/allschool/CuadroDialogo.java
@@ -1,14 +1,11 @@
-package com.example.tareas;
+package com.example.allschool;
 
 import android.app.Dialog;
-import android.content.ContentValues;
 import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class CuadroDialogo {
 
@@ -29,7 +26,7 @@ public class CuadroDialogo {
         final EditText materia = (EditText)dialogo.findViewById(R.id.et_materia);
         final EditText descrpcion = (EditText)dialogo.findViewById(R.id.et_descripcion);
         Button btn = (Button)dialogo.findViewById(R.id.btn);
-
+        
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/allschool/Tareas.java
+++ app/src/main/java/com/example/allschool/Tareas.java
@@ -1,22 +1,25 @@
 package com.example.allschool;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class Tareas extends AppCompatActivity {
+public class Tareas extends AppCompatActivity implements CuadroDialogo.FinalizoCuadroDialogo{
 
     private ListView tareas;
-    private  EditText et_materia;
-    private ImageButton btn;
-    private  EditText et_descripcion;
     Context contexto;
     ArrayList<String> arrayList;
     ArrayAdapter<String> adapter;
@@ -26,25 +29,64 @@ public class Tareas extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tareas);
 
-        et_materia = (EditText)findViewById(R.id.et_materia);
-        et_descripcion = (EditText)findViewById(R.id.et_desc);
-        btn = (ImageButton)findViewById(R.id.btn);
+        contexto = this;
+
         tareas = (ListView)findViewById(R.id.lista_tareas);
 
         arrayList = new ArrayList<String>();
+
+        AdminSQLiteOpenHelper juego = new AdminSQLiteOpenHelper(this, "jumm", null, 2);
+        SQLiteDatabase bd = juego.getWritableDatabase();
+        Cursor fila = bd.rawQuery("select materia, descripcion from tareas", null);
+        if(fila.moveToFirst()){
+            do{
+                arrayList.add("• " + fila.getString(0) + "\n" + fila.getString(1));
+            }while(fila.moveToNext());
+        }
+        bd.close();
+
         adapter = new ArrayAdapter<String>(Tareas.this, android.R.layout.simple_list_item_1, arrayList);
         tareas.setAdapter(adapter);
     }
 
-    public void Adicionar(View view){
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String materia = et_materia.getText().toString();
-                String descripcion = et_descripcion.getText().toString();
-                arrayList.add(materia + "\n" + descripcion);
-                adapter.notifyDataSetChanged();
-            }
-        });
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menuacciones, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.adicionar){
+            item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    new CuadroDialogo(contexto, Tareas.this);
+                    return false;
+                }
+            });
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void ResultadoCuadroDialogo(String materia, String descripcion) {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this, "jumm", null, 2);
+        SQLiteDatabase db = admin.getWritableDatabase();
+
+        if (!materia.isEmpty() && !descripcion.isEmpty()) {
+            ContentValues registro = new ContentValues();
+            registro.put("materia", materia);
+            registro.put("descripcion", descripcion);
+
+            db.insert("tareas", null, registro);
+            Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "Debe registrar todos los campos", Toast.LENGTH_SHORT).show();
+        }
+
+        db.close();
+        finish();
+        startActivity(getIntent());
     }
 }
